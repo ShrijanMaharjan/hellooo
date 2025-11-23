76cafc4b4be97ac2e693c298ddf488a62a75d64e
ShrijanMaharjan <146512001+ShrijanMaharjan@users.noreply.github.com>
2025-11-23 17:53:11 +0545

    Delete LabExam directory

--- LabExam/displayproducts.js
+++
@@ -1,8 +0,0 @@
-import { products } from "./products.js";
-
-export function displayProducts() {
-  console.log("PRODUCT LIST:");
-  products.forEach((p) => {
-    console.log(`${p.id}. ${p.name} - $${p.price}`);
-  });
-}
